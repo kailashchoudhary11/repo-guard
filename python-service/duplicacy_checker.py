890bc2a8dd16d2282778d186b01c9e8191cd5ebe
Kailash Choudhary <kailashjat604@gmail.com>
2024-06-23 14:21:39 +0530

    feat: add python service to check duplicacy

---
+++ python-service/duplicacy_checker.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+from sentence_transformers import SentenceTransformer, util
+
+app = Flask(__name__)
+model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+
+@app.route('/compare_issues', methods=['POST'])
+def compare_issues():
+    data = request.json
+    issue1_title = data.get('issue1_title')
+    issue1_body = data.get('issue1_body')
+    issue2_title = data.get('issue2_title')
+    issue2_body = data.get('issue2_body')
+
+    if not issue1_title or not issue1_body or not issue2_title or not issue2_body:
+        return jsonify({"error": "Invalid input"}), 400
+
+    issue1_text = issue1_title + " " + issue1_body
+    issue2_text = issue2_title + " " + issue2_body
+
+    embedding1 = model.encode(issue1_text, convert_to_tensor=True)
+    embedding2 = model.encode(issue2_text, convert_to_tensor=True)
+    cosine_similarity = util.pytorch_cos_sim(embedding1, embedding2)
+
+    return jsonify({"similarity": cosine_similarity.item()}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
