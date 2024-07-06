70afe99704002d366d65f8a9d0faaa32ace8003d
Kailash Choudhary <kailashjat604@gmail.com>
2024-07-06 15:16:16 +0530

    remove unused endpoint

--- main.go
+++ main.go
@@ -8,8 +8,6 @@ import (
 
 	"github.com/kailashchoudhary11/repo-guard/handlers"
 	"github.com/kailashchoudhary11/repo-guard/initializers"
-	"github.com/kailashchoudhary11/repo-guard/models"
-	"github.com/kailashchoudhary11/repo-guard/services"
 	"github.com/kailashchoudhary11/repo-guard/templates"
 )
 
@@ -27,16 +25,6 @@ func main() {
 	})
 
 	router.HandleFunc("/webhook", handlers.Webhook)
-	router.HandleFunc("/issues", func(w http.ResponseWriter, r *http.Request) {
-		repo := models.Repository{
-			Name: "Flipkart_Clone",
-			Owner: models.User{
-				Username: "arghadipmanna101",
-			},
-		}
-		issues := services.FetchIssues(initializers.GithubClient, repo)
-		fmt.Println("The number of issues are ", len(issues))
-	})
 	fmt.Println("Service is up and running at port 8000")
 	http.ListenAndServe(":8000", router)
 }
